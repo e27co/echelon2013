7cc00e12f3e8bcfa7d0df2a79227a7173a3c7b95
Jairus Bondoc <jairus@e27.sg>
2013-02-21 12:00:09 +0800

    button on testinmonials

--- sidetestimonials.php
+++ sidetestimonials.php
@@ -3,7 +3,7 @@
 <div class="green-quote no-float">quote</div>
 <p class="quote">Being an Echelon sponsor was not only promote my product but it means, you're supporting the startup ecosystem in the Region.</p>
 <p class="auth">- Hugh Mason<br><em>Co-founder - JFDI Asia</em></p>
-<a href="" alt="Sponsor" title="Be an e27 Sponsor">Sponsor</a>
+<a href="" alt="Sponsor" title="Be an e27 Sponsor" class="btn btn-success btn-large">Be a Sponsor</a>
 </div>
 <div class="side-pillar-partners">
 <div class="green-quote no-float">quote</div>
