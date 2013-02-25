07ca6d2df95dc61b43b6de6dcfd4464b9d91b943
Jay Carreon <mjcarreon@gmail.com>
2013-02-25 19:53:33 +0800

    Navigation Menu update

--- js/functions.js
+++ js/functions.js
@@ -17,7 +17,47 @@
 
        
        
+       //Persistent Header
+    function UpdateTableHeaders() {
+       $(".persist-area").each(function() {
        
+           var el             = $(this),
+               offset         = el.offset(),
+               scrollTop      = $(window).scrollTop(),
+               floatingHeader = $(".floatingHeader", this)
+           
+           if ((scrollTop > offset.top)) {
+               floatingHeader.css({
+                "visibility": "visible"
+               });
+           } else {
+               floatingHeader.css({
+                "visibility": "hidden"
+               });      
+           };
+       });
+    }
+    
+    // DOM Ready      
+    $(function() {
+    
+       var clonedHeaderRow;
+    
+       $(".persist-area").each(function() {
+           clonedHeaderRow = $(".persist-header", this);
+           clonedHeaderRow
+             .before(clonedHeaderRow.clone())
+             .css("width", clonedHeaderRow.width())
+             .addClass("floatingHeader");
+             
+       });
+       
+      $(window)
+        .scroll(UpdateTableHeaders)
+        .trigger("scroll");
+       
+    });
+    //Persistent eader end
         
 	
 
