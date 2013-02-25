ec78c82bc711aeef111973e67fc7738a83b638f5
Jay Carreon <mjcarreon@gmail.com>
2013-02-25 22:27:36 +0800

    Menu plugin

--- js/functions.js
+++ js/functions.js
@@ -17,28 +17,7 @@
 
        
        
-       //Persistent Header
-    function UpdateTableHeaders() {
-       $(".persist-area").each(function() {
-       
-           var el             = $(this),
-               offset         = el.offset(),
-               scrollTop      = $(window).scrollTop(),
-               floatingHeader = $(".floatingHeader", this)
-           
-           if ((scrollTop > offset.top)) {
-               floatingHeader.css({
-                "visibility": "visible"
-               });
-           } else {
-               floatingHeader.css({
-                "visibility": "hidden"
-               });      
-           };
-       });
-    }
-    
-    // DOM Ready      
+       //Persistent Header   
     $(function() {
     
        var clonedHeaderRow;
